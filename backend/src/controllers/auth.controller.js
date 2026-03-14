73cb4c1f0d064a70a8510d3d805e449f7288c07e
Sanjay Rana <sanjayrana5113@gmail.com>
2026-03-14 16:18:19 +0530

    logout error

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -98,7 +98,7 @@ export const login = async (req, res) => {
 };
 
 export const logout = (_, res) => {
-  res.cookie("jwt", "", { maxAge: 0 });
+  res.cookie("talkSpace", "", { maxAge: 0 });
   res.status(200).json({ message: "Logged out successfully" });
 };
 
