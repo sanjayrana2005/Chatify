959839df9f3b2fdd0b9756f0bbdae1aa414d995c
Sanjay Rana <sanjayrana5113@gmail.com>
2026-03-14 16:02:30 +0530

    env error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : `${import.meta.VITE_API_URL}/api`,
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
 });
